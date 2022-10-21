f7006417a90c6c77f4802d18eec86310983049f2
Akash Chhabria <akashrai10.ar10@gmail.com>
2022-10-21 12:21:55 -0400

    1.7: unicafe: step 2

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 
-const Display = ({ text,value }) => (
-  <p>{text}: {value}</p>
+const Display = ({ text,value,symbol }) => (
+  <p>{text}: {value} {symbol}</p>
 )
 
 const Button = ({ handleClick, text }) => (
@@ -15,18 +15,26 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+  const [avg, setAvg] = useState(0)
 
-  const handleGood = () => (
+  const handleGood = () => {
+    setAll(all+1)
     setGood(good + 1)
-  )
+    setAvg(avg+1)
+  }
 
-  const handleNeutral = () => (
+  const handleNeutral = () => {
+    setAll(all+1)
     setNeutral(neutral + 1)
-  )
+    setAvg(avg+0)
+  }
 
-  const handleBad = () => (
+  const handleBad = () => {
+    setAll(all+1)
     setBad(bad + 1)
-  )
+    setAvg(avg-1)
+  }
   
 
   return (
@@ -39,11 +47,14 @@ const App = () => {
       </div>
       <h1>Statistics</h1>
       <div>
-        <Display text='good' value={good}/>
-        <Display text='neutral' value={neutral}/>
-        <Display text='bad' value={bad}/>
-      </div>
+        <Display text='good' value={good} symbol={' '}/>
+        <Display text='neutral' value={neutral} symbol={' '}/>
+        <Display text='bad' value={bad} symbol={' '}/>
+        <Display text='all' value={all} symbol={' '}/>
+        <Display text='average' value={avg/all} symbol={' '}/>
+        <Display text='positive' value={(good/all)*100} symbol={'%'}/>
 
+      </div>
     </div>
   )
 }
